1494e4a10c560496ed380e4e49af8daf38dcf246
edcastillob <edwar.castillo@gmail.com>
2023-10-01 14:32:20 -0400

    Build

--- src/git-data-user/git-data-user.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GitDataUserService } from './services/git-data-user.service';
-import { GitDataUserController } from './controllers/git-data-user.controller';
-
-@Module({
-  providers: [GitDataUserService],
-  controllers: [GitDataUserController]
-})
-export class GitDataUserModule {}
